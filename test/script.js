725c2033af9641ab306fc98a84a7def33984bcc2
Illia Rudakov <ilugarudakov@gmail.com>
2019-02-28 17:13:18 +0200

    First commit

---
+++ test/script.js
@@ -0,0 +1,16 @@
+'use stict';
+// console.log(1);
+
+// let person = {
+//     name: 'John',
+//     age: 18,
+//     isMarried: true
+// }
+
+// console.log(person ['name'] );
+
+// let arr = ['one', 'two', 'three'];
+// console.log(arr[2])
+
+let answer = prompt ( "Вопрос?" );
+console.log ( answer );
