20baac908ff18e78aa8a6a33d73b0f706efb7641
Illia Rudakov <ilugarudakov@gmail.com>
2019-03-01 12:36:17 +0200

    '/e/JavaScript/02 Основы JavaScript/008. Задание 1. Подготавливаем проект к дальнейшим урокам'

--- test/script.js
+++ test/script.js
@@ -1,7 +1,22 @@
 'use stict';
+ let money = +prompt("Ваш бюджет на месяц?", "");
+ let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-if ( < 2) {
-    console.log("1 < 2");
-} else {
-    console.log(" nevrno ")
-}
+var appData = {
+    money,
+    timeData: time,
+    expenses,
+    optionalExpenses:{},
+    income: "",
+    savings: false
+};
+
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let answer2 = +prompt("Во сколько обойдется?", "");
+
+var expenses = {};
+expenses [answer1] = answer2;
+appData.expenses = expenses;
+
+console.log ("Ваш ежедневный бюджет: " + (appData.money - appData.expenses[answer1]) / 30 );
+
